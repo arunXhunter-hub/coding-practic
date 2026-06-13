7ae3f81330102cb9d2f7620f266cd5ba80769adf
arunXhunter-hub <arundakhore123@gmail.com>
2026-06-13 14:37:24 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is Even"
+    else:
+        return f"{number} is Odd"
 
+
+print(check_even_odd(7))
+print(check_even_odd(10))
